417c73ab5cb2e5aae046efa1fc3f980df601e6a8
unknown <nicolassam33@gmail.com>
2023-03-15 11:27:07 +0100

    first commit

---
+++ _includes/navigation.php
@@ -0,0 +1,10 @@
+<!-- Navigation -->
+<nav class="navBarre">
+    <ul>
+        <li class="menu"><img src="/" alt=""></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <title>Wild Cinema - Home</title>
+</head>
+<body>
+<?php
+include "_includes/navigation.php";
+?>
+<?php
+include "_includes/slide.php";
+?>
+
+
+
+<?php
+include "_includes/footer.php";
+?>
+
+</body>
+</html>
