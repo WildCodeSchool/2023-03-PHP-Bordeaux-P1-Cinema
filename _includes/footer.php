d045d0a0597ee47a37cd218812c3d6fcde790822
CAROLINE LEBON <lebon.caroline.lc@gmail.com>
2023-03-16 11:06:50 +0100

    initial commit for test

---
+++ _includes/footer.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+
+<html>
+<!-- VERSION DESKTOP : FLECHE - choisir : &#9650 flèche noire ou &#9651 flèche blanche - et copyright ;
+en CSS flèche en haut et copyright en bas tous les deux centrés avec infos réglementaires sur la droite et boutons -->
+
+<p>&#9650</p>
+<p>&#9651</p>
+<p>&copy</p>
+
+<!--Version MOBILE page HOME (index.php): en ligne en bas.
+Version MOBILE page FILMS (ex. : terminator.php): 
+Version DESKTOP : n'apparaît pas-->
+<br>
+<!--A CORRIGER : correspond à la NAVBAR, ne concerne pas le FOOTER-->
+<ul class="links">
+    <li><img src="#" alt="Haut de la page"></li>
+    <li><img src="#" alt="Films"></li>
+    <li><img src="#" alt="Logo"></li>
+    <li><img src="#" alt="Tickets"></li>
+    <li><img src="#" alt="Contact"></li>
+</ul>
+
+<!-- Version DESKTOP: ces infos sont sur la gauche
+Version MOBILE page FILMS : apparaît au-dessus de la
+ NAVBAR et en-dessous PROMO-->
+<ul class="regulations">
+    <li>&copy The Wild Cinema</li>
+    <li><a href="#">CGU</a></li>
+    <li><a href="#">CHARTE</a></li>
+    <li><a href="#">POLITIQUE</a></li>
+</ul>
+
+<!-- Version DESKTOP: ces infos sont sur la droite-->
+<ul>
+    <li><img src="#" alt="Facebook"></li>
+    <li><img src="#" alt="Twitter"></li>
+    <li><img src="#" alt="Instagram"></li>
+</ul>
+</html>
+
