caf9cf99f1d9e8a4a3ce0f703ef55bd9a93817ac
EdouardH <ehsurfdkr@hotmail.com>
2023-03-18 15:29:45 +0100

    Final version, add all links, add active search box

--- _includes/navigation.php
+++ _includes/navigation.php
@@ -1,30 +1,40 @@
 <!-- Barre de Navigation -->
-<nav class="navBar">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="assets/css/modules/cf-navigation.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 
-    <!-- Le LOGO du haut-->
-    <a href="#" class="firstLogo">LOGO</a>
-
-    <!-- Partie barre de recherche + boutton OK-->
-    <div>
-        <form action="#" method="get">
-            <input type="text" name="search" placeholder="Recherche">
-            <button type="submit">OK</button>
-        </form>
+</head>
+<body>
+<nav class="navbar">
+    <div class="logo">
+        <img src="assets/images/Logo definitif.png" alt="Logo">
     </div>
-
-    <!-- Partie des liens mennant aux différentes pages-->
-    <ul class="navLinks">
-        <li><a class= "accueil" href="#">Accueil</a></li>
-        <li class= "bouttonup"><a href="#">%</a></li>
-        <li><a href="#">Films</a></li>
-
-        <!--Deuxième logo-->
-        <li class="secondlogo"><a href="#">LOGO2</a> </li>
-
-        <li><a href="#">Horaires</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
-<!--Fermeture-->
+    <div class="search">
+        <form>
+            <input type="text" id="search-box" placeholder="Rechercher...">
+            <button type="submit" id="search-btn">OK</button>
+            <script src="assets/javaScript/searchBox.js"></script>
+        </form>
+        <ul class="results">
+            <!-- Résultats de la recherche seront ajoutés dynamiquement ici -->
+        </ul>
+    </div>
+    <div class="links">
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Films</a></li>
+            <img src="assets/images/Logo definitif.png" alt="Logo" class="logo2">
+            <li><a href="#">Horaires</a></li>
+            <li><a href="#">Contacts</a></li>
+        </ul>
+    </div>
 </nav>
+</body>
+<!--Fin de Barre de Navigation-->
 
-<!-- header -->
+
+<!--Placement du header ici -->
