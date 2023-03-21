b7a8cd88c8fc2b1ae79d10cbf1a9763716fa3f90
ETIENNE_JOUAULT <etienne.jouaulmt@live.fr>
2023-03-21 15:25:01 +0100

    schedule modification

--- schedule.php
+++ schedule.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="assets/css/modules/cf-variables.css">
     <link rel="stylesheet" href="assets/css/modules/cf-navigation.css">
     <link rel="stylesheet" href="assets/css/modules/cf-about.css">
-    <link rel="stylesheet" href="assets/css/modules/cf-schedule.css">
+    <link rel="stylesheet" href="/assets/css/modules/cf-schedule.css">
     <link rel="stylesheet" href="assets/css/modules/cf-footer.css">
 </head>
 <body>
