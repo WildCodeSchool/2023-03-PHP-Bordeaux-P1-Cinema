8f2c108ba2eb10a5bc01412e88a09a9510b9a535
EdouardH <ehsurfdkr@hotmail.com>
2023-03-22 09:38:38 +0100

    update searchBox display

--- _includes/navigation.php
+++ _includes/navigation.php
@@ -1,10 +1,7 @@
+<!-- Barre de Navigation -->
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="../assets/css/modules/cf-navigation.css">
-=======
->>>>>>> 7718865170125dd7ed2be703480536100e761afd
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
@@ -15,35 +12,6 @@
     <link rel="stylesheet" href="../assets/css/modules/cf-footer.css">
 
 </head>
-<<<<<<< HEAD
-
-<nav class="navbar"> <!-- Barre de Navigation -->
-    <div class="logo">
-        <img src="../assets/images/logoDefinitif.png" alt="Logo">
-    </div>
-    <div class="search">
-        <form>
-            <input class= "searchInput" type="text" id="search-box" placeholder="Rechercher...">
-            <button type="submit" onclick="return search; id="search-btn">OK</button>
-            <script src="../assets/javaScript/searchBox.js"></script>
-        </form>
-        <ul class="results">
-            <!-- Résultats de la recherche seront ajoutés dynamiquement ici -->
-        </ul>
-    </div>
-    <div class="links">
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="movies.php">Films</a></li>
-            <img src="assets/images/logoDefinitif.png" alt="Logo" class="logo2" href="index.php">
-            <li><a href="schedule.php">Horaires</a></li>
-            <li><a href="about.php">Contacts</a></li>
-        </ul>
-    </div>
-</nav><!--Fin de Barre de Navigation-->
-<header>
-
-=======
 <body>
 <!-- header -->
 <header id="home">
@@ -54,7 +22,7 @@
         </div>
         <div class="search">
             <form>
-                <input type="text" id="search-box" placeholder="Rechercher...">
+                <input class= "search-box" type="text" id="search-box" placeholder="Rechercher...">
                 <button type="submit" id="search-btn">OK</button>
                 <script src="../assets/javaScript/searchBox.js"></script>
             </form>
@@ -73,4 +41,3 @@
         </div>
     </nav><!--Fin de navbar-->
 </header><!-- fin de header -->
->>>>>>> 7718865170125dd7ed2be703480536100e761afd
