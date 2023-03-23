c4e8b11526e170c1538a5aefce30ee91ff966982
ETIENNE_JOUAULT <etienne.jouaulmt@live.fr>
2023-03-23 12:52:11 +0100

    modif JS and added footer

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,28 +1,22 @@
-<!--<footer>-->
+<div class="regulations">
     <!--------------Legal request--------------->
-        <!--<div class="regulations">
             <ul>
                 <li class="regulationsOne"><a href="#">CGU</a></li>
                 <li class="regulationsTwo"><a href="#">CHARTE</a></li>
                 <li class="regulationsThree"><a href="#">POLITIQUE</a></li>
             </ul>
-        </div>-->
+        </div>
     <!------------End of legal request------------------>
-
-        <!-- DESKTOP: à gauche ; MOBILE : absence-->
-        <!--<div class="socialNetworks">
+        <div class="socialNetworks">
             <ul>
                 <li><a href="https://www.facebook.com"><img src="../assets/images/facebook-removebg-preview.png" alt="Facebook" height=30px></a></li>
                 <li><a href="https://twitter.com/?lang=fr"><img src="../assets/images/twitter-removebg-preview.png" alt="Twitter" height=30px></a></li>
                 <li><a href="https://www.instagram.com"><img src="../assets/images/instagram-removebg-preview.png" alt="Instagram" height=30px></a></li>
             </ul>
-        </div>-->
-    <!-- DESKTOP : copyright (en bas centré) ; MOBILE page CONTACT : centré entre .regulations et NAVBAR-->
-
-    <!--<div>
+        </div>
         <p class="copyrightFooter">&copy The Wild Cinema</p>
     </div>
-</footer>-->
+</footer>
 <script src="../assets/js/script.js"></script>
 </body>
 </html>
--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,68 @@ if(window.matchMedia("(min-width: 1200px)").matches){
 else if (window.matchMedia("(min-width: 10px)").matches) {
     homeBackground.style.backgroundImage="url('" + rand + "Small.jpg')";
 }
+
+/*---------SEARCHBOX---------*/
+
+
+function search(e) {
+
+
+
+    var pages=Array(
+        ["Avatar","avatar.php"],
+        ["avatar","avatar.php"],
+        ["AVATAR","avatar.php"],
+        ["creed 3","creed3.php"],
+        ["creed three","creed3.php"],
+
+        ["Creed three","creed3.php"],
+        ["creed Three","creed3.php"],
+        ["Creed Three","creed3.php"],
+        ["CREED THREE","creed3.php"],
+        ["CREED 3","creed3.php"],
+        ["mon crime","mon_crime.php"],
+        ["Mon crime","mon_crime.php"],
+        ["mon Crime","mon_crime.php"],
+        ["MON CRIME","mon_crime.php"],
+        ["puss in boots","pussinboots.php"],
+        ["Puss in boots","pussinboots.php"],
+        ["puss in Boots","pussinboots.php"],
+        ["Puss in Boots","pussinboots.php"],
+        ["PUSS IN BOOTS","pussinboots.php"],
+        ["scream 6","scream6.php"],
+        ["Scream 6","scream6.php"],
+        ["SCREAM 6","scream6.php"],
+        ["scream six","scream6.php"],
+        ["scream Six","scream6.php"],
+        ["Scream Six","scream6.php"],
+        ["Scream six","scream6.php"],
+
+        ["SCREAM SIX","scream6.php"]    );
+    let page=document.getElementById("pages").value;
+    for(let i=0;i<pages.length;i++) {
+        if(page==pages[i][0]) {
+            window.location.href=pages[i][1];
+            return false;
+        }
+    }
+    window.location.href="error.php";
+    return false;
+}
+
+["SCREAM SIX","scream6.php"]
+
+
+
+
+);
+var page=document.getElementById("page").value;
+for(var i=0;i<pages.length;i++) {
+    if(page==pages[i][0]) {
+        window.location.href=pages[i][1];
+        return false;
+    }
+}
+window.location.href="error.php";
+return false;
+}
